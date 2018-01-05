bd81967b3b72382a45eedd38110cf9856948d937
Ajusa <arhamjain@gmail.com>
2018-01-04 22:05:35 -0500

    many updates
    added card component, renamed .input to .card, and added a watch to the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -15,3 +15,19 @@ function css(done) {
         .pipe(gulp.dest('dist/'));
     done();
 }
+gulp.task('minify', minify);
+
+function minify(done) {
+    var postcss = require('gulp-postcss');
+    var cleanCSS = require('gulp-clean-css');
+    var cssvariables = require('postcss-css-variables');
+    return gulp.src('src/*.css')
+        .pipe(postcss([cssvariables()]))
+        .pipe(cleanCSS({ level: 2 }))
+        .pipe(gulp.dest('dist/'))
+    done();
+}
+gulp.task('watch', function(){
+    gulp.series('minify')
+    gulp.watch("src/*.css").on('change', gulp.series('minify'));
+})
