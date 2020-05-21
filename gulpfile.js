6336fc15762e1ca644d0cb3126c1f94d371e7756
Arham Jain <arhamjain@gmail.com>
2020-05-21 10:11:12 -0400

    fixed issue with 6 col, and updated dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
 var gulp = require('gulp');
-
+var gzip = require('gulp-gzip');
 gulp.task('default', css);
 
 function css(done) {
     var postcss = require('gulp-postcss');
     var cleanCSS = require('gulp-clean-css');
     var cssvariables = require('postcss-css-variables');
-    var zopfli= require('gulp-zopfli');
     return gulp.src('src/*.css')
         .pipe(postcss([cssvariables()]))
         .pipe(cleanCSS({ level: 2 }))
         .pipe(gulp.dest('dist/'))
-        .pipe(zopfli())
-        .pipe(gulp.dest('dist/'));
+        .pipe(gzip())
+        .pipe(gulp.dest('dist/'))
     done();
 }
 gulp.task('minify', minify);
